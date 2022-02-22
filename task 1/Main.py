f9b445989d2a43393071780a6dcc8ca156274371
Tom_C <19695450@students.lincoln.ac.uk>
2022-02-22 23:08:01 +0000

    done ?

--- task 1/Main.py
+++ task 1/Main.py
@@ -2,9 +2,12 @@ import numpy
 import pandas as pd
 import matplotlib.pyplot
 from matplotlib import pyplot as plt
+import matplotlib.ticker as plticker
 import numpy as np
-from sklearn.metrics import mean_squared_error
-
+import math
+def mean_squared_error(y_tester,Regress):
+    mse = np.square(np.subtract(y_tester,Regress)).mean()
+    return mse
 def Pol_Matrix(features_train, degree):
   X = np.ones(features_train.shape)
   for i in range(1,degree + 1):
@@ -28,11 +31,13 @@ def pol_regression(x_train, y_train, degree):
          w1 = W_Pol_Fit(x_train, y_train, degree)
     Xtest1 = Pol_Matrix(x_train, degree)
     ytest1 = Xtest1.dot(w1)
+    print(w1)
     print
     plt.plot(x_train, ytest1, 'r')
 
-    plt.legend(('training points', 'ground truth', '$x$'), loc = 'lower right')
-
+    plt.legend(('training points', 'poly line', '$x$'), loc = 'lower right')
+    title = "polynomial regression " +  str(degree)
+    plt.title(title)
     plt.savefig('polynomial.png')
     plt.show()
     return ytest1;
@@ -42,37 +47,35 @@ data_file = data_file.sort_values(by=['x'], ascending=True)
 
 x = data_file['x']
 y = data_file['y']
+height = len(x)
+x_trainer = x[0:math.floor(height*0.7)]
+y_trainer = y[0:math.floor(height*0.7)]
 
-x_trainer = x.sample(frac=0.7,random_state=0)
-y_trainer = y.sample(frac=0.7,random_state=0)
+x_tester = x[0:math.ceil(height*0.3)]
+y_tester = y[0:math.ceil(height*0.3)]
 
-x_tester = x.drop(x_trainer.index)
-y_tester = y.drop(y_trainer.index)
-
-Regress0 = pol_regression(x_trainer,y_trainer,0)
-print(Regress0)
-
-Regress1 = pol_regression(x_trainer,y_trainer,1)
-
-print(Regress1.shape)
-
-print(y_tester.shape)
-Regress1.resize((6),refcheck=False)
-print(Regress1)
-print(np.sqrt(mean_squared_error(y_tester,Regress1)))
-
+Regress0 = pol_regression(x,y,0)
+Regress1 = pol_regression(x,y,1)
 Regress2 = pol_regression(x,y,2)
-Regress2.resize((6),refcheck=False)
-print(np.sqrt(mean_squared_error(y_tester,Regress2)))
 Regress3 = pol_regression(x,y,3)
-Regress3.resize((6),refcheck=False)
-print(np.sqrt(mean_squared_error(y_tester,Regress3)))
-#print(np.sqrt(mean_squared_error(y_tester,Regress3)))
 Regress4 = pol_regression(x,y,6)
-Regress4.resize((6),refcheck=False)
-print(np.sqrt(mean_squared_error(y_tester,Regress4)))
 Regress5 = pol_regression(x,y,10)
-Regress5.resize((6),refcheck=False)
-print(np.sqrt(mean_squared_error(y_tester,Regress5)))
 
+RsmeTest = []
+RsmeTrain = []
+for i in range(11):
+    Regress0 = pol_regression(x_trainer,y_trainer,i)
+    RsmeTrain.append(np.sqrt(mean_squared_error(y_trainer,Regress0)))
+    Regress0.resize((6),refcheck=False)
+    RsmeTest.append(np.sqrt(mean_squared_error(y_tester,Regress0)))
 
+print(RsmeTrain)
+print(RsmeTest)
+plt.plot(RsmeTrain, color='r')
+plt.xticks([0,1,2,3,6,10])
+plt.yticks(np.arange(0, 80, step=5))
+
+plt.plot(RsmeTest,color='b')
+plt.legend(('Train regression','Test regression', 'poly line', '$x$'), loc='upper right')
+
+plt.show()
--- task 3/Main.py
+++ task 3/Main.py
@@ -11,6 +11,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report,confusion_matrix
 from sklearn import metrics
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score
 def graph(data):
     patient = data.loc[data['Participant Condition'] == "Patient"]
     patient = patient[["Alpha"]]
@@ -29,8 +31,10 @@ def graph(data):
 
     control = data_file.loc[data['Participant Condition'] != "Patient"]
     control = control[["Beta"]]
-    fig = sns.kdeplot(control["Beta"], shade=True, color="g")
     fig = sns.kdeplot(patient["Beta"], shade=True, color="r")
+    fig = sns.kdeplot(control["Beta"], shade=True, color="g")
+
+    plt.legend((' patient', ' control ', '$x$'), loc='lower right')
     plt.show()
 def sksplit(data,k):
     kf = KFold(n_splits=k,shuffle=True)
@@ -46,47 +50,156 @@ print(data_file.describe().transpose())
 X = data_file[Prd].values
 y = data_file[Tc].values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=40)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=40) # data splitter
 
-print(X_train.shape); print(X_test.shape)
-mlp = MLPClassifier(hidden_layer_sizes=(50,50), activation='relu', solver='adam', max_iter=500)
-mlp2 = MLPClassifier(hidden_layer_sizes=(500,500), activation='relu', solver='adam', max_iter=500)
-mlp3 = MLPClassifier(hidden_layer_sizes=(1000,1000), activation='relu', solver='adam', max_iter=500)
-
-#mlp.fit(X_train,y_train)
-
-#mlp2.fit(X_train,y_train)
-
-#mlp3.fit(X_train,y_train)
-
-#predict_train = mlp.predict(X_train)
-#predict_train2 = mlp2.predict(X_train)
-#predict_train3 = mlp3.predict(X_train)
-#predict_test = mlp.predict(X_test)
-#predict_test2 = mlp2.predict(X_test)
-#predict_test3 = mlp3.predict(X_test)
-#print(confusion_matrix(y_train,predict_train))
+print(X_train.shape)
+print(X_test.shape)
+print(X_train)
+mlp = MLPClassifier(hidden_layer_sizes=(500,500), activation='logistic', solver='lbfgs', max_iter=500)
 
 
-#print(classification_report(y_train,predict_train))
-#print(confusion_matrix(y_train,predict_train2))
-#print(classification_report(y_train,predict_train2))
-#print(confusion_matrix(y_train,predict_train3))
-#print(classification_report(y_train,predict_train3))
-#clf = RandomForestClassifier(n_estimators=50,min_samples_leaf=10)
-#clf2 = RandomForestClassifier(n_estimators=500,min_samples_leaf=10)
-#clf3 = RandomForestClassifier(n_estimators=10000,min_samples_leaf=10)
-#clf.fit(X_train,y_train)
-#clf2.fit(X_train,y_train)
-#clf3.fit(X_train,y_train)
-#y_pred = clf.predict(X_test)
-#print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
-#y_pred2 = clf2.predict(X_test)
-#print("Accuracy:",metrics.accuracy_score(y_test, y_pred2))
-#y_pred3 = clf3.predict(X_test)
-#print("Accuracy:",metrics.accuracy_score(y_test, y_pred3))
+mlp.fit(X_train,y_train)
+predict_train = mlp.predict(X_train)
+predict_test = mlp.predict(X_test)
+print(confusion_matrix(y_train,predict_train))
+
+
+print(classification_report(y_train,predict_train))
+
+clf = RandomForestClassifier(n_estimators=1000,min_samples_leaf=10)
+
+clf.fit(X_train,y_train)
+
+y_pred = clf.predict(X_test)
+print("Accuracy 10 leaf :",metrics.accuracy_score(y_test, y_pred))
+clf = RandomForestClassifier(n_estimators=1000,min_samples_leaf=5)
+
+clf.fit(X_train,y_train)
+
+y_pred = clf.predict(X_test)
+print("Accuracy 5 leaf :",metrics.accuracy_score(y_test, y_pred))
+## 3.3 below
 k = 10
-SplitData= sksplit(data_file,k)
-print(SplitData[0])
+df = data_file.drop(labels=['Image number','Bifurcation number','Artery (1)/ Vein (2)'], axis=1)
+out = df["Participant Condition"].transpose()
+print(df)
+inp = df.drop(['Participant Condition'], axis=1)
+kf = KFold(n_splits=10)
+kf.get_n_splits(X)
+print(kf)
+inp = inp.transpose()
+for train_index,test_index in kf.split(df):
+    X_train, X_test = inp[train_index],inp[test_index]
+    y_train, y_test = out[train_index], out[test_index]
 
-#predict_train = mlp.predict(X_train)
+X_train = X_train.transpose()
+X_test = X_test.transpose()
+y_train = y_train.transpose()
+y_test = y_test.transpose()
+
+AnnClassOne = MLPClassifier(hidden_layer_sizes=(50,50), activation='logistic', solver='adam', max_iter=5000)
+AnnClassTwo = MLPClassifier(hidden_layer_sizes=(500,500), activation='logistic', solver='adam', max_iter=5000)
+AnnClassThree = MLPClassifier(hidden_layer_sizes=(1000,1000), activation='logistic', solver='adam', max_iter=5000)
+TreeForestOne = RandomForestClassifier(n_estimators=50,min_samples_leaf=10)
+TreeForestTwo = RandomForestClassifier(n_estimators=500,min_samples_leaf=10)
+TreeForestThree = RandomForestClassifier(n_estimators=10000,min_samples_leaf=10)
+
+AS = []
+for train_index,test_index in kf.split(df):
+    X_train, X_test = inp[train_index], inp[test_index]
+    y_train, y_test = out[train_index], out[test_index]
+    X_train = X_train.transpose()
+    X_test = X_test.transpose()
+    y_train = y_train.transpose()
+    y_test = y_test.transpose()
+    AnnClassOne.fit(X_train,y_train)
+    predict_train = AnnClassOne.predict(X_train)
+    predict_test = AnnClassOne.predict(X_test)
+    AS.append(accuracy_score(y_train, predict_train))
+scores = cross_val_score(AnnClassOne, inp.transpose(), out.transpose(), cv=5)
+print('Average accuracy 50 ANN')
+print("CV accuracy = %0.2f accuracy with a standard deviation of %0.2f" % (scores.mean(), scores.std()))
+print("Normal Accuracy = ",sum(AS)/len(AS))
+AS = []
+for train_index,test_index in kf.split(df):
+    X_train, X_test = inp[train_index], inp[test_index]
+    y_train, y_test = out[train_index], out[test_index]
+    X_train = X_train.transpose()
+    X_test = X_test.transpose()
+    y_train = y_train.transpose()
+    y_test = y_test.transpose()
+    AnnClassTwo.fit(X_train,y_train)
+    predict_train = AnnClassTwo.predict(X_train)
+    predict_test = AnnClassTwo.predict(X_test)
+    AS.append(accuracy_score(y_train, predict_train))
+print('Average accuracy 500 ANN')
+scores = cross_val_score(AnnClassTwo, inp.transpose(), out.transpose(), cv=10)
+print("CV accuracy = %0.2f accuracy with a standard deviation of %0.2f" % (scores.mean(), scores.std()))
+print("Normal Accuracy = ",sum(AS)/len(AS))
+
+AS = []
+for train_index,test_index in kf.split(df):
+    X_train, X_test = inp[train_index], inp[test_index]
+    y_train, y_test = out[train_index], out[test_index]
+    X_train = X_train.transpose()
+    X_test = X_test.transpose()
+    y_train = y_train.transpose()
+    y_test = y_test.transpose()
+    AnnClassThree.fit(X_train,y_train)
+    predict_train = AnnClassThree.predict(X_train)
+    predict_test = AnnClassThree.predict(X_test)
+    AS.append(accuracy_score(y_train, predict_train))
+
+print('Average accuracy 10000 ANN')
+scores = cross_val_score(AnnClassThree, inp.transpose(), out.transpose(), cv=5)
+print("CV accuracy = %0.2f accuracy with a standard deviation of %0.2f" % (scores.mean(), scores.std()))
+print("Normal Accuracy = ",sum(AS)/len(AS))
+
+for train_index,test_index in kf.split(df):
+    X_train, X_test = inp[train_index], inp[test_index]
+    y_train, y_test = out[train_index], out[test_index]
+    X_train = X_train.transpose()
+    X_test = X_test.transpose()
+    y_train = y_train.transpose()
+    y_test = y_test.transpose()
+    TreeForestOne.fit(X_train,y_train)
+    predict_train = TreeForestOne.predict(X_train)
+    predict_test = TreeForestOne.predict(X_test)
+    AS.append(accuracy_score(y_train, predict_train))
+
+print('Average accuracy Tree 50')
+scores = cross_val_score(TreeForestOne, inp.transpose(), out.transpose(), cv=5)
+print("CV accuracy = %0.2f accuracy with a standard deviation of %0.2f" % (scores.mean(), scores.std()))
+print("Normal Accuracy = ",sum(AS)/len(AS))
+for train_index,test_index in kf.split(df):
+    X_train, X_test = inp[train_index], inp[test_index]
+    y_train, y_test = out[train_index], out[test_index]
+    X_train = X_train.transpose()
+    X_test = X_test.transpose()
+    y_train = y_train.transpose()
+    y_test = y_test.transpose()
+    TreeForestTwo.fit(X_train,y_train)
+    predict_train = TreeForestTwo.predict(X_train)
+    predict_test = TreeForestTwo.predict(X_test)
+    AS.append(accuracy_score(y_train, predict_train))
+
+print('Average accuracy Tree 500')
+scores = cross_val_score(TreeForestTwo, inp.transpose(), out.transpose(), cv=5)
+print("CV accuracy = %0.2f accuracy with a standard deviation of %0.2f" % (scores.mean(), scores.std()))
+print("Normal Accuracy = ",sum(AS)/len(AS))
+for train_index,test_index in kf.split(df):
+    X_train, X_test = inp[train_index], inp[test_index]
+    y_train, y_test = out[train_index], out[test_index]
+    X_train = X_train.transpose()
+    X_test = X_test.transpose()
+    y_train = y_train.transpose()
+    y_test = y_test.transpose()
+    TreeForestThree.fit(X_train,y_train)
+
+    predict_train = TreeForestThree.predict(X_train)
+    predict_test = TreeForestThree.predict(X_test)
+    AS.append(accuracy_score(y_train, predict_train))
+print('Average accuracy Tree 10000')
+scores = cross_val_score(TreeForestThree, inp.transpose(), out.transpose(), cv=5)
+print("CV accuracy = %0.2f accuracy with a standard deviation of %0.2f" % (scores.mean(), scores.std()))
+print("Normal Accuracy = ",sum(AS)/len(AS))
